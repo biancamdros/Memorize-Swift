4c6b11134c0c00fb9890229d5632ce2ae607eb87
biancamdros <bibilaise@gmail.com>
2023-02-08 15:39:02 -0300

    Adicionando cards

--- Memorize.Swift/ContentView.swift
+++ Memorize.Swift/ContentView.swift
@@ -2,18 +2,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View{
-        return HStack(content:{
-            ForEach(0..<4, content: {index in 
-                ZStack(content: {
-                    RoundedRectangle(cornerRadius:10.0).stroke()
-                    RoundedRectangle(cornerRadius:10.0).fill(Color.white)
-                    Text("<3")
-            })
-        })
-    })
+        HStack{
+            ForEach(0..<4) {index in 
+                CardView()
+        }
+    }
         .padding()
         .foregroundColor(Color.orange)
         .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View{
+    var isFaceUp: Bool
+
+    var body: some View{
+        ZStack {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius:10.0).stroke()
+                RoundedRectangle(cornerRadius:10.0).fill(Color.white)
+                Text("<3")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+            
+            
+        }
     }
 }
 
